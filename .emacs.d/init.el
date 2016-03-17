41811a2a31aa71898090af7cd95758d645f3054c
Zak B. Elep <zakame@drift.zakame.net>
2016-03-17 16:28:17 +0800

    Set up Emacs for JavaScript!

---
+++ .emacs.d/init.el
@@ -0,0 +1,428 @@
+;;; init.el --- an Emacs configuration for JavaScript programming
+;; Copyright (C) 2016 Zak B. Elep, <zakame@zakame.net>
+
+;;; Commentary:
+
+;; This is a minimal(?  Really now...) Emacs configuration for my talk
+;; on Emacs + JavaScript :) Adapted from my own personal `init.el'.
+
+;; Usage
+
+;; Put this file inside your `user-emacs-directory' (typically
+;; `~/.emacs.d').
+
+;; TODO (Left for the reader ;)
+
+;; - Switch from ido to Ivy or Helm for more consistent completion
+;; - Add a few more conveniences (multiple-cursors, hungry-delete,
+;;   expand-region)
+
+;;; Code:
+
+;;; General setup
+
+;; Enable mouse wheel support
+(if (fboundp 'mwheel-install) (mwheel-install))
+
+;; Disable menu, tool, and scroll bars
+(mapc (lambda (mode)
+        (when (fboundp mode) (funcall mode -1)))
+      '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+
+(blink-cursor-mode -1)		        ; Stop blinking the cursor/point
+(setq load-prefer-newer t)              ; Always load newer elisp
+(setq enable-local-eval t)              ; Tell Emacs to obey variables
+                                        ; set by the files it reads
+(setq visible-bell t)                   ; Blink the screen instead of
+                                        ; beeping
+(set-language-environment "UTF-8")      ; Set my default language
+                                        ; environment
+(windmove-default-keybindings)          ; Enable windmove
+(winner-mode 1)                         ; Enable winner-mode
+(auto-image-file-mode 1)                ; Show images as images, not as
+                                        ; semi-random bits
+(setq inhibit-startup-message t)        ; No splash screen (well...)
+(if (fboundp 'fringe-mode) (fringe-mode 0)) ; no fringes too, please!
+
+;; package.el
+(require 'package)
+(add-to-list 'package-archives
+       '("melpa" . "https://melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; use-package
+(unless (package-installed-p 'use-package)
+  (progn
+    (package-refresh-contents)
+    (package-install 'use-package)))
+(eval-when-compile
+  (eval-after-load 'advice
+    `(setq ad-redefinition-action 'accept))
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+
+;;; Editing/Programming
+
+;; I want backups in their own directory, and even backup while in VC
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name "backups" user-emacs-directory)))
+      vc-make-backup-files t)
+
+;; Disable backups for TRAMP files, though
+(add-to-list 'backup-directory-alist
+             (cons tramp-file-name-regexp nil))
+
+(global-font-lock-mode 1)
+(setq font-lock-support-mode 'jit-lock-mode) ; Just In Time font-locking
+(setq font-lock-maximum-decoration t)
+
+(line-number-mode 1)                    ; Show line number ...
+(column-number-mode 1)                  ; ... and column number on
+                                        ; modeline
+(show-paren-mode 1)                     ; Automatically makes the
+                                        ; matching parenthesis stand out
+                                        ; in color
+(setq show-paren-style 'expression)     ; Make the entire matched expression
+                                        ; stand out
+(mouse-avoidance-mode 'cat-and-mouse)   ; Move the mouse pointer out
+                                        ; of my way when I type
+(temp-buffer-resize-mode 1)             ; Temporary windows should not
+                                        ; get into our way
+(auto-compression-mode 1)               ; Load Auto-(De)Compression Mode
+(setq next-line-add-newlines nil)         ; This disables down-arrow and
+                                        ; C-n at the end of a buffer
+                                        ; from adding a new line to that
+                                        ; buffer
+
+(setq search-whitespace-regexp ".*?")	; match anything (non-greedy)
+
+(setq auto-save-timeout 15              ; Auto-save after 15 sec of
+                                        ; idleness
+      require-final-newline t           ; Always add a newline to file's end
+      search-highlight t                ; Highlight search strings
+      compilation-window-height 10      ; Set a small window for
+                                        ; compiles
+      compilation-scroll-output
+      'first-error                      ; Follow compilation scrolling
+                                        ; until the first error
+      compilation-ask-about-save nil)
+
+;; Use imenu to browse use-package blocks
+(defun zakame/imenu-use-package ()
+  "Extract use-package lines to be used as anchors in imenu."
+  (add-to-list 'imenu-generic-expression
+               '(nil
+                 "\\(^\\s-*(use-package +\\)\\(\\_<.+\\_>\\)" 2)))
+(add-hook 'emacs-lisp-mode-hook #'zakame/imenu-use-package)
+
+;; ido-mode
+(use-package ido
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (setq ido-use-virtual-buffers t
+        ido-use-filename-at-point 'guess
+        ido-create-new-buffer 'always
+        ido-ignore-extensions t))
+
+;; ido-ubiquitous
+(use-package ido-ubiquitous
+  :ensure t
+  :config
+  (ido-ubiquitous-mode 1))
+
+;; flx-ido
+(use-package flx-ido
+  :ensure t
+  :config
+  (flx-ido-mode 1)
+  (setq ido-enable-flex-matching t)
+  (setq ido-use-faces nil))
+
+;; ido-vertical-mode
+(use-package ido-vertical-mode
+  :ensure t
+  :config
+  (ido-vertical-mode 1)
+  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
+
+;; idomenu
+(use-package idomenu
+  :ensure t
+  :bind (("C-x c i" . idomenu)))
+
+;; ido-other-window
+(use-package ido-other-window
+  :load-path "site-lisp/ido-other-window")
+
+;; smex
+(use-package smex
+  :ensure t
+  :bind (("M-x" . smex)
+	 ("M-X" . smex-major-mode-commands))
+  :config
+  (smex-initialize))
+
+;; Save point position between editing sessions
+(use-package saveplace
+  :config
+  (setq-default save-place t
+                save-place-file (expand-file-name ".places"
+                                                  user-emacs-directory)))
+
+;; Auto refresh buffers and dired, and be quiet about it
+(global-auto-revert-mode 1)
+(setq global-auto-revert-non-file-buffers t
+      auto-revert-verbose nil)
+
+;; dired-x
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")))
+
+;; set human-readable sizes in dired
+(setq dired-listing-switches "-alh")
+
+;; recentf tweaks
+(use-package recentf
+  :config
+  (defun zakame/ido-recentf-open ()
+    "Use `ido-completing-read' to find a recent file."
+    (interactive)
+    (find-file (ido-completing-read "Find recent file: " recentf-list nil t)))
+  (bind-key "C-c f" 'zakame/ido-recentf-open)
+  (recentf-mode)
+  (setq recentf-exclude '("TAGS" ".*-autoloads\\.el\\'")))
+
+;; Ansi-Term tweaks
+(use-package term
+  :bind (("C-c t" . ansi-term))
+  :config
+  (defadvice term-sentinel (around ansi-term-kill-buffer (proc msg))
+    (if (memq (process-status proc) '(signal exit))
+	(let ((buffer (process-buffer proc)))
+	  ad-do-it
+	  (kill-buffer buffer))
+      ad-do-it))
+  (ad-activate 'term-sentinel)
+  (defadvice ansi-term (before ansi-term-force-shell)
+    (interactive (list (getenv "SHELL"))))
+  (ad-activate 'ansi-term)
+  (add-hook 'term-mode-hook 'goto-address-mode)
+  (add-hook 'term-exec-hook
+	    '(lambda ()
+	       (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))))
+
+;; Eshell tweaks
+(use-package eshell
+  :bind (("C-c e" . eshell))
+  :config
+  (defun zakame/eshell-rename-buffer-before-command ()
+    (let* ((last-input
+            (buffer-substring eshell-last-input-start eshell-last-input-end)))
+      (rename-buffer
+       (format "*eshell[%s]$ %s...*" default-directory last-input) t)))
+  (defun zakame/eshell-rename-buffer-after-command ()
+    (rename-buffer
+     (format "*eshell[%s]$ %s*" default-directory
+             (eshell-previous-input-string 0)) t))
+  (add-hook 'eshell-pre-command-hook
+            'zakame/eshell-rename-buffer-before-command)
+  (add-hook 'eshell-post-command-hook
+            'zakame/eshell-rename-buffer-after-command)
+  (use-package em-smart)
+  (setq eshell-where-to-jump 'begin
+        eshell-review-quick-commands nil
+        eshell-smart-space-goes-to-end t)
+  (add-hook 'eshell-mode-hook
+            (lambda ()
+              (eshell-smart-initialize))))
+
+;; make window splits much smarter especially when on widescreen
+(defun zakame/split-window-prefer-side-by-side (window)
+  "Split WINDOW, preferably side by side."
+  (let ((split-height-threshold (and (< (window-width window)
+				      split-width-threshold)
+				   split-height-threshold)))
+    (split-window-sensibly window)))
+(setq split-window-preferred-function
+      #'zakame/split-window-prefer-side-by-side)
+
+;; undo-tree
+(use-package undo-tree
+  :diminish undo-tree-mode
+  :ensure t
+  :config
+  (global-undo-tree-mode 1))
+
+;; hippie-exp
+(use-package hippie-exp
+  :config
+  (global-set-key (kbd "M-/") 'hippie-expand)
+  (setq hippie-expand-try-functions-list
+        '(
+          try-expand-dabbrev
+          try-expand-dabbrev-all-buffers
+          try-complete-file-name-partially
+          try-complete-file-name
+          try-expand-all-abbrevs
+          try-expand-list
+          try-expand-line
+          try-complete-lisp-symbol-partially
+          try-complete-lisp-symbol)))
+
+;; diff-hl
+(use-package diff-hl
+  :ensure t
+  :config
+  (global-diff-hl-mode)
+  (setq diff-hl-side 'left)
+  (diff-hl-margin-mode))
+
+;; aggressive indent
+(use-package aggressive-indent
+  :diminish aggressive-indent-mode
+  :ensure t
+  :config
+  (add-to-list 'aggressive-indent-excluded-modes 'jade-mode)
+  (global-aggressive-indent-mode 1))
+
+;; which-key-mode
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :config
+  (which-key-mode))
+
+;; adaptive wrap for long lines
+(use-package adaptive-wrap
+  :diminish visual-line-mode
+  :ensure t
+  :config
+  (add-hook 'text-mode-hook #'visual-line-mode)
+  (add-hook 'text-mode-hook #'adaptive-wrap-prefix-mode))
+
+;; Enable ElDoc for automatic documentation of elisp functions
+(dolist (hook
+         '(emacs-lisp-mode-hook lisp-interaction-mode-hook ielm-mode-hook))
+  (add-hook hook #'eldoc-mode))
+
+;; Don't mention ElDoc mode in modeline
+(eval-after-load "eldoc"
+  '(diminish 'eldoc-mode))
+
+;; Always indent using spaces, no tabs
+(setq-default indent-tabs-mode nil)
+
+
+;;; Org-mode
+
+(use-package org
+  :ensure t
+  :mode ("\\.\\(org\\|org_archive\\)$" . org-mode)
+  :bind (("\C-cl" . org-store-link)
+         ("\C-cc" . org-capture)
+         ("\C-ca" . org-agenda)
+         ("\C-cb" . org-iswitchb))
+  :config
+  ;; make windmove work well with org-mode
+  (add-hook 'org-shiftup-final-hook 'windmove-up)
+  (add-hook 'org-shiftleft-final-hook 'windmove-left)
+  (add-hook 'org-shiftdown-final-hook 'windmove-down)
+  (add-hook 'org-shiftright-final-hook 'windmove-right)
+  ;; add some langs to to babel
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((js . t)
+     (sh . t)))
+  ;; other org tweaks
+  (setq org-ellipsis "▼"
+        org-src-fontify-natively t
+        org-src-preserve-indentation nil
+        org-edit-src-content-indentation 0))
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; ob-http (REST client)
+(use-package ob-http
+  :after org
+  :ensure t
+  :config
+  (add-to-list 'org-babel-load-languages '(http . t))
+  (org-babel-do-load-languages
+   'org-babel-load-languages org-babel-load-languages))
+
+;; htmlize
+(use-package htmlize
+  :defer t
+  :ensure t)
+
+;; org-present (for meetups)
+(use-package org-present
+  :bind (("C-c m" . org-present))
+  :ensure t
+  :config
+  (add-hook 'org-present-mode-hook
+            (lambda ()
+              (org-present-big)
+              (org-display-inline-images)))
+  (add-hook 'org-present-mode-quit-hook
+            (lambda ()
+              (org-present-small)
+              (org-remove-inline-images))))
+
+
+;;; JavaScript and other modes for this talk is all in the slides!
+
+(package-initialize nil)
+(org-babel-load-file "~/README.org")
+
+
+;;; Misc
+
+;; Plan9 theme (I <3 Acme)
+(use-package plan9-theme
+  :ensure t
+  :config
+  (add-hook 'after-make-frame-functions
+	    (lambda (frame)
+	      (with-selected-frame frame
+		(load-theme 'plan9 t)))))
+
+;; Emojis! :+1:
+(use-package emojify
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-emojify-mode))
+
+;; Nyan-mode :3
+(use-package nyan-mode
+  :ensure t
+  :config
+  (nyan-mode 1))
+
+;; zone out with Nyan cat when idle after 2 minutes :3
+(use-package zone-nyan
+  :ensure t
+  :preface
+  (use-package zone)
+  :config
+  (setq zone-programs [zone-nyan])
+  (zone-when-idle 120))
+
+;; Set up a cozy fireplace
+(use-package fireplace
+  :ensure t
+  :config
+  (delete-other-windows)
+  (fireplace t))
+
+(provide 'init)
+
+;;; init.el ends here
