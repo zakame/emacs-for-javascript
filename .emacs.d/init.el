ad31a48ac60ab9067e7eeb4ba090cc725ff404a2
Zak B. Elep <zakame@drift.zakame.net>
2016-03-21 12:23:03 +0800

    Allow emojify to get images at install-time

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -435,6 +435,8 @@
 (use-package emojify
   :ensure t
   :config
+  (unless (file-exists-p emojify-image-dir)
+    (emojify-download-emoji emojify-emoji-set))
   (add-hook 'after-init-hook #'global-emojify-mode))
 
 ;; Nyan-mode :3
