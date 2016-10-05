70b73375cca1c26cf6f5eb0c3879fad10878fee3
Zak B. Elep <zakame@drift.zakame.net>
2016-10-05 12:19:49 +0800

    Put local customizations in separate file
    Especially to prevent Emacs from making changes to `init.el` on its
    own (via Custom settings or package.el updates.)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -431,6 +431,10 @@
                  (delete-other-windows)
                  (fireplace t)))))
 
+;;; Local customizations
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
 (provide 'init)
 
